85a2e751ae0ecca0ef9bbdd868fcb6ffe5bdc153
daviguerradonascimento <daviguerrapolar@gmail.com>
2025-03-09 23:33:13 -0300

    recatoring code

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import GameComponent from "./game/GameComponent.jsx";
 
+function App() {
+  return <GameComponent />;
+}
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<GameComponent />);
+root.render(<App />);
